c9d48a3ce10a3b5a7bb2a98304ac99ba0da45a62
SuperJim <526554176@qq.com>
2025-03-25 10:55:54 +0800

    Test clerk sign in and sign out.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>Sign in</Button>
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
